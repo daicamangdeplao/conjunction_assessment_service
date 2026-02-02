8b3d8585e02dc891c88369a322c3f9b1a931e8f1
Trung Dung Nguyen <trungdung.nguyen.000000@gmail.com>
2026-02-02 17:02:15 +0100

    add admin controller for testing

---
+++ src/main/java/org/codenot/ssa/controller/AdminController.java
@@ -0,0 +1,26 @@
+package org.codenot.ssa.controller;
+
+import org.codenot.ssa.service.background.RoutineScreeningScheduler;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@Profile("dev")
+public class AdminController {
+
+    private final RoutineScreeningScheduler routineScreeningScheduler;
+
+    public AdminController(RoutineScreeningScheduler routineScreeningScheduler) {
+        this.routineScreeningScheduler = routineScreeningScheduler;
+    }
+
+    @GetMapping("/scheduler")
+    public ResponseEntity<String> runSchedulerSync() {
+        routineScreeningScheduler.submitRoutineScreeningManual();
+        return ResponseEntity.ok("Scheduler is running");
+    }
+}
